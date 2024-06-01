59e39e6a29dd6abb701e23ef8741401085043db0
AnsariAhmedFrontEnde <am27043@gmail.com>
2024-06-01 19:11:10 +0530

    Experiance

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import styles from './App.module.css'
 import  Hero  from './components/Hero/Hero'
 import Navbar from './components/Navbar/Navbar';
 import About from './components/About/About';
+import Experiance from './components/Experiance/Experiance';
 
 function App() {
   
@@ -12,6 +13,7 @@ function App() {
       <Navbar />
       <Hero />
       <About />
+      <Experiance />
     </div>
   )
 }
