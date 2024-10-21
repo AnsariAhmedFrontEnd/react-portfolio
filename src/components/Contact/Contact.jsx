d5469b2b0960028c61b2a50557949067b035d248
AnsariAhmedFrontEnd <am27043@gmail.com>
2024-10-21 14:41:14 +0530

    Added new project

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -7,7 +7,7 @@ const Contact = () => {
   return (
     <footer id="contact" className={styles.container}>
       <div className={styles.text}>
-        <h2>Contact</h2>
+        <h3>Contact</h3>
         <p>Feel free to reach out!</p>
       </div>
       <ul className={styles.links}>
--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -7,16 +7,19 @@ const ProjectCard = ({project : {imageSrc, title, description, demo, source, ski
               <img src={getImageUrl(imageSrc)} alt={`Image of ${title}`}  className={styles.image}/>
               <h3 className={styles.title}>{title}</h3>
               <p className={styles.description}>{description}</p>
+              <div>
               <ul className={styles.skills}>
                 {skills.map((skill, id) => {
                   return <li key={id} className={styles.skill}>{skill}</li>;
                 })}
               </ul>
+              
               <div className={styles.links}>
                 <a href={demo} className={styles.link}>Demo</a>
                 <a href={source} className={styles.link}>Source</a>
               </div>
-            </div>
+              </div>
+              </div>
     )
 };
 
